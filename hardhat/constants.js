176cbeafa52915f6c0b61d05c1bb7460c182ab84
Carlos Mendoza <kaziel@gmail.com>
2022-03-12 17:01:28 -0600

    Contracts/hardhat section complete

---
+++ hardhat/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0xb030b03CDce8B2a217067777D7D0CB187D5c513E";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
